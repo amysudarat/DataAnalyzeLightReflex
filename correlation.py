07489dae0c69f715159060766d2b58559da63b6b
Amy <amy.sudarat@gmail.com>
2018-10-21 02:16:56 -0400

    finish scatter plot and get correlation coefficient matrix

--- correlation.py
+++ correlation.py
@@ -33,30 +33,50 @@ corr_df["LUXmeter"] = LUXonAvg["LUX"]
 More detail: https://stackoverflow.com/questions/4688715/find-time-shift-between-two-similar-waveforms
 """
 # Drop NaN
-#corr_df = corr_df.dropna()
-#corr_df["Kinect"] = corr_df["Kinect"].astype(float)
-#corr_df["LUXmeter"] = corr_df["LUXmeter"].astype(float)
-delay_index_kl = np.argmax(signal.correlate(corr_df["Kinect"],corr_df["LUXmeter"]))-len(corr_df)-1
-delay_index_lk = np.argmax(signal.correlate(corr_df["LUXmeter"],corr_df["Kinect"]))-len(corr_df)-1
-#delay_index_lk = np.argmax(signal.correlate(corr_df["LUXmeter"],["Kinect"]))-len(corr_df.index)
+corr_df_dropna = corr_df.dropna()
+# Find delay by performing cross correlation
+delay_index_kl = np.argmax(signal.correlate(corr_df_dropna["Kinect"],corr_df_dropna["LUXmeter"]))-len(corr_df_dropna)-1
+delay_index_lk = np.argmax(signal.correlate(corr_df_dropna["LUXmeter"],corr_df_dropna["Kinect"]))-len(corr_df_dropna)-1
+
 print(delay_index_kl)
 print(delay_index_lk)
 
-""" Shift Signal Time """ # result in shifting 5 from the above section
+""" Shift Signal Time """ 
 k = corr_df.drop(columns=['LUXmeter'])
 l = corr_df.drop(columns=['Kinect'])
 plotData.plotLUXcompared(l,k,'plot corr_df before shifting','Kinect')
 
-k = k.shift(periods=-delay_index)
+k = k.shift(periods=delay_index_lk)
 
 plotData.plotLUXcompared(l,k,'after shifting kinect shift','Kinect')
 
-k = k.shift(periods=5)
-l = l.shift(periods=1)
-plotData.plotLUXcompared(l,k,'after shifting lux shift','Kinect')
+k['LUXmeter'] = l['LUXmeter']
+
+""" Find Linear correlation Pearson """ 
+k_dropna = corr_df_dropna.drop(columns=['LUXmeter'])
+l_dropna = corr_df_dropna.drop(columns=['Kinect'])
+l_dropna = l_dropna.shift(periods=delay_index_lk)
+k_dropna['LUXmeter'] = l_dropna['LUXmeter']
+print(l.head(3))
+
+# full signals
+corr = k.corr(method='pearson', min_periods=1)
+m,b = np.polyfit(k_dropna['Kinect'],k_dropna['LUXmeter'],1)
+#fit_fn = np.poly1d(fit)
+
+# drop Nan signals
+corr_dropna = k_dropna.corr(method='pearson', min_periods=1)
+#plt.matshow(a.corr(method='pearson'))
+print("Correlation full signal")
+print(corr)
+print("Correlation drop NaN")
+print(corr_dropna)
+plt.scatter(k_dropna['Kinect'],k_dropna['LUXmeter'],c='blue')
+
+plt.scatter(k['Kinect'],k['LUXmeter'],c='red')
+plt.plot(k['Kinect'],(m*k['Kinect'])+b,'r')
 
 
- 
 #"""Normalization"""
 #x = corr_df["Kinect"].values #returns a numpy array
 #min_max_scaler = preprocessing.MinMaxScaler()
